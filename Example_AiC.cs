c6c8241a1906534b849840c763d6cc1de90dbd45
Amir <Amir.boudani@gmail.com>
2020-11-11 18:23:57 +0100

    changes to the files

--- Example_AiC.cs
+++ Example_AiC.cs
@@ -10,25 +10,23 @@ using AmbientAICallouts.API;
 namespace Example_AiC
 {
     //Its very important to use these unless the AiCallout will not be compatible with the player callouts
-    ///Vector3 location;
-    ///String callSign;
-    ///String calloutDetailsString;
-    ///Voicelines rndVl;
-    ///Vehicle unit;
+    ///String SceneInfo
+    ///Vector3 location
+    ///String callSign
+    ///String calloutDetailsString
+    ///Voicelines rndVl
+    ///Vehicle unit
     ///List<Ped> unitOfficers
     ///List<Ped> suspects
     ///List<Vehicle> suspectsVehicle
 
     //Please use try catch blocks to get an error message when chrashing.
-    ///For almost every API Function you will need an so called "functions Object". It knows to current managerObject which contains most of the features in AiCalloutManager. The object name is always "fO"
-    //-------------------------->>> Important: Always use fO when calling functions to ensure you are using the right managerObject <<<-------------------------
-
     public class Example_AiC : AiCallout
     {
         public override bool Setup()
         {
             //Code for setting the scene. return true when Succesfull. 
-            //Important: please set a calloutDetailsString with Set_AiCallout_calloutDetailsString(String calloutDetailsString) to ensure that your callout has a something a civilian can report.
+            //Important: Please set a calloutDetailsString and SceneInfo to ensure that your callout has a something a civilian can report.
             //Example idea: Place a Damaged Vehicle infront of a Pole and place a swearing ped nearby.
             try
             {
@@ -56,35 +54,43 @@ namespace Example_AiC
                 }
                 else  //if vehicle is reaching its location
                 {
+
                     GameFiber.WaitWhile(() => Unit.Position.DistanceTo(location) >= 40f, 0);
                     Unit.IsSirenSilent = true;
                     Unit.TopSpeed = 12f;
 
                     GameFiber.SleepUntil(() => location.DistanceTo(Unit.Position) < arrivalDistanceThreshold + 5f /* && Unit.Speed <= 1*/, 30000);
-                    Unit.Driver.Tasks.PerformDrivingManeuver(VehicleManeuver.Wait);
-                    GameFiber.SleepUntil(() => Unit.Speed <= 1, 5000);
                     OfficersLeaveVehicle(true);
 
-                    if (IsAiTakingCare()) //do something without involving the player.
+                    if (playerRespondingInAdditon) //if the player responds as a additional unit to the AiCallout
                     {
-                        //doStuff
+                        // doStuff 
+                        // when player is also responding
                     }
-                    else //do something and involve the player. 
-                    {
-                        
-                        //decision maker what callout shall triggered
-                        switch (new Random().Next(0, 5))                                                                                                           //FOR VIDEO EDITING
+                    else //if the player is not responding as additional
+                    { 
+
+                        if (IsAiTakingCare()) //do something without calling the player for backup
                         {
-                            case 0:
-                                UnitCallsForBackup("AAIC-OfficerDown");
-                                break;
-                            case 1:
-                                UnitCallsForBackup("AAIC-OfficerInPursuit");
-                                break;
-                            default:
-                                UnitCallsForBackup("AAIC-OfficerRequiringAssistance");
-                                break;
-                        }                    
+                            //doStuff
+                        }
+                        else //do something and call the player for backup
+                        {
+                        
+                            //decision tree what callout shall get triggered
+                            switch (new Random().Next(0, 5))                                                                                                           //FOR VIDEO EDITING
+                            {
+                                case 0:
+                                    UnitCallsForBackup("AAIC-OfficerDown");
+                                    break;
+                                case 1:
+                                    UnitCallsForBackup("AAIC-OfficerInPursuit");
+                                    break;
+                                default:
+                                    UnitCallsForBackup("AAIC-OfficerRequiringAssistance");
+                                    break;
+                            }                    
+                        }
                     }
                 }
                 return true;
